c4b57b1cd2b49060c803a7c0b27126e0000d63c7
ajith chadaga <ajith1892>
2021-11-20 18:19:38 +0530

    changes

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running and how are u"
+    return "Flask app is running and how are u and i am doing good thanks "
 
 port = int(os.getenv("PORT", 5001))
 
